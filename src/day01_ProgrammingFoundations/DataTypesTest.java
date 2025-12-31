8c75fd7c51f3fd1efebdfcf1605f62f370071a9c
Munna Aziz Mondal <azizmunna661@gmail.com>
2025-12-31 11:31:34 +0530

    Day 01, Data Types

---
+++ src/day01_ProgrammingFoundations/DataTypesTest.java
@@ -0,0 +1,26 @@
+package day01_ProgrammingFoundations;
+
+/*
+2. Variables & Data Types
+Interview Questions
+--------------------
+    1. Difference between local, instance, and static variables
+    2. Default values for data types
+    3. Why boolean size is JVM-dependent?
+    4. Why char uses 2 bytes in Java?
+
+ */
+
+
+public class DataTypesTest {
+    static int a;
+    boolean flag;
+
+    public static void main(String[] args) {
+        int b;
+
+       // System.out.println(b); // why error
+
+        System.out.println(a);  // default value 0
+    }
+}
