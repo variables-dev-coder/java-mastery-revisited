f164ba7b27ff390ae9dcaeee8f3992ea53c4f7f4
Munna Aziz Mondal <azizmunna661@gmail.com>
2026-01-01 13:03:53 +0530

    Day 02, Method Overloding example

---
+++ src/day02_Core_Java_Fundamentals/Calculation.java
@@ -0,0 +1,28 @@
+package day02_Core_Java_Fundamentals;
+
+// Method overloading
+
+public class Calculation {
+
+    int add(int a, int b) {
+        return a + b;
+    }
+
+    double add(double a, double b) {
+        return a + b;
+    }
+
+    int add(int a, int b, int c) {
+        return a + b + c;
+    }
+
+    public static void main(String[] args) {
+        Calculation c = new Calculation();
+        
+        System.out.println(c.add(2, 3));         // 6
+
+        System.out.println(c.add(2.5, 3.5));     // 6.0
+
+        System.out.println(c.add(1, 2, 3));   // 6
+    }
+}
