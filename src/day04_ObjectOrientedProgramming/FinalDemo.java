9f94c62b0773621bfb739e4ea50dd5da87cbd943
Munna Aziz Mondal <azizmunna661@gmail.com>
2026-01-04 19:27:11 +0530

    Day 04, Final method example

---
+++ src/day04_ObjectOrientedProgramming/FinalDemo.java
@@ -0,0 +1,18 @@
+package day04_ObjectOrientedProgramming;
+
+
+class A2 {
+    final void display() {
+        System.out.println("Final method");
+    }
+}
+
+public class FinalDemo {
+    public static void main(String[] args) {
+        A2 a2 = new A2();
+        a2.display();
+    }
+}
+
+
+// Final method
