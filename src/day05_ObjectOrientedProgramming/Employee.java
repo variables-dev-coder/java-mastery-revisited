a5d3080eedc5d03bbe7012a9a60d18aa872ecf6e
Munna Aziz Mondal <azizmunna661@gmail.com>
2026-01-06 15:44:39 +0530

    Day 05, Abstract Class – Shared State + Behavior example

---
+++ src/day05_ObjectOrientedProgramming/Employee.java
@@ -0,0 +1,34 @@
+package day05_ObjectOrientedProgramming;
+
+abstract class Employee {
+    protected String name;
+
+    Employee(String name) {
+        this.name = name;
+    }
+
+    abstract double calculateSalary();
+
+    void printName() {
+        System.out.println("Employee: " + name);
+    }
+}
+
+class FullTimeEmployee extends Employee {
+    FullTimeEmployee(String name) {
+        super(name);
+    }
+
+    double calculateSalary() {
+        return 50000;
+    }
+
+    public static void main(String[] args) {
+        Employee e = new FullTimeEmployee("Munna");
+        e.printName();
+        System.out.println(e.calculateSalary());
+    }
+}
+
+//Employee: Munna
+//50000.0
