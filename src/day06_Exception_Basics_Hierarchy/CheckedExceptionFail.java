12ef662062e3a4c2512ca4647bdf01ac971e5fa1
Munna Aziz Mondal <azizmunna661@gmail.com>
2026-01-08 22:12:22 +0530

    Day 07, Checked Exception – Compilation Failure

---
+++ src/day06_Exception_Basics_Hierarchy/CheckedExceptionFail.java
@@ -0,0 +1,33 @@
+package day06_Exception_Basics_Hierarchy;
+
+/*
+Checked Exception – Compilation Failure (Core Proof)
+Concept
+Checked exceptions are enforced by the compiler.
+Code (Compilation Fails)
+ */
+
+import java.io.FileReader;
+
+public class CheckedExceptionFail {
+
+    public static void main(String[] args) {
+        
+       // FileReader reader = new FileReader("data.txt");
+    }
+}
+
+// Unhandled exception: java.io.FileNotFoundException
+
+/*
+Explanation
+FileReader declares it may throw FileNotFoundException
+This is a checked exception
+Compiler forces handling because:
+    File system is external
+    JVM cannot guarantee safety
+
+Interview Line
+
+Checked exceptions represent risks outside JVM control.
+ */
