f561c02920a07031a7cb98c828f8e9d9d3ac3459
Munna Aziz Mondal <azizmunna661@gmail.com>
2026-01-14 18:37:13 +0530

    Day 09, INVALID Multi-catch (Parent + Child)

---
+++ src/day09_Multiple_catch_Multi_catch/InvalidMultiCatch.java
@@ -0,0 +1,18 @@
+package day09_Multiple_catch_Multi_catch;
+
+public class InvalidMultiCatch {
+//    public static void main(String[] args) {
+//
+//        try {
+//            int a = 10 / 0;
+//        }
+//        catch (Exception |  ArithmeticException e) {
+//            System.out.println("Error");
+//        }
+//    }
+}
+
+
+// NOT COMPILE
+
+// Exception already covers ArithmeticException
