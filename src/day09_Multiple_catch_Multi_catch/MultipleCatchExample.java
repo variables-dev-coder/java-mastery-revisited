2a45b7ac2f0464d245512a5fd89fa4aa90111481
Munna Aziz Mondal <azizmunna661@gmail.com>
2026-01-14 17:07:47 +0530

    Day 09, Multiple Catch Blocks (Different Handling)

---
+++ src/day09_Multiple_catch_Multi_catch/MultipleCatchExample.java
@@ -0,0 +1,20 @@
+package day09_Multiple_catch_Multi_catch;
+
+public class MultipleCatchExample {
+    public static void main(String[] args) {
+        try {
+            int[] arr = {1, 2};
+            System.out.println(arr[5]); // ArrayIndexOutOfBoundsException
+        }
+        catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Array index error handled");
+        }
+        catch (ArithmeticException e) {
+            System.out.println("Arithmetic error handled");
+        }
+        catch (NullPointerException e) {
+            System.out.println("Null pointer handled");
+        }
+    }
+}
+
