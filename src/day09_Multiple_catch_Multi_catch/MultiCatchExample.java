4a8b1635cdb59c457c25f0f61a21640e401a3a49
Munna Aziz Mondal <azizmunna661@gmail.com>
2026-01-14 18:30:44 +0530

    Day 09, Multi-catch (|) – Same Handling Logic

---
+++ src/day09_Multiple_catch_Multi_catch/MultiCatchExample.java
@@ -0,0 +1,18 @@
+package day09_Multiple_catch_Multi_catch;
+
+public class MultiCatchExample {
+    public static void main(String[] args) {
+        try {
+            int[] arr = {1, 2};
+            System.out.println(arr[5]);
+        }
+        catch (NullPointerException |
+               ArithmeticException |
+               ArrayIndexOutOfBoundsException e) {
+
+            System.out.println("Runtime exception occurred");
+        }
+    }
+}
+
+// Runtime exception occurred
