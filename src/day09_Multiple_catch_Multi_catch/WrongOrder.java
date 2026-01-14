71c8718797ccfc12bd4471209b5f97200df2f434
Munna Aziz Mondal <azizmunna661@gmail.com>
2026-01-14 17:13:41 +0530

    Day 09, Order of Catch Blocks (Child → Parent)

---
+++ src/day09_Multiple_catch_Multi_catch/WrongOrder.java
@@ -0,0 +1,21 @@
+package day09_Multiple_catch_Multi_catch;
+
+public class WrongOrder {
+
+ //   public static void main(String[] args) {
+
+//        try {
+//            int a = 10 / 0;
+//        }
+//        catch (Exception e) {
+//            System.out.println("General");
+//        }
+//        catch (ArithmeticException e) {
+//            System.out.println("Arithmetic");
+//        }
+//    }
+}
+
+// WRONG Order (WILL NOT COMPILE)
+
+// Error: Unreachable catch block
