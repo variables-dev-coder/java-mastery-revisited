20009de15f80087cf81b5dc799869e73c406d113
Munna Aziz Mondal <azizmunna661@gmail.com>
2026-01-16 19:05:25 +0530

    Day 10, throw vs throws Exception Propagation

---
+++ src/day10_throwVSthrows/PropagationPractice.java
@@ -0,0 +1,25 @@
+package day10_throwVSthrows;
+
+//Observe exception traveling up the stack
+
+public class PropagationPractice {
+
+    static void level3() {
+        int a = 10 / 0;
+    }
+
+    static void level2() {
+        level3();
+    }
+
+    static void level1() {
+        level2();
+    }
+
+    public static void main(String[] args) {
+        level1();
+    }
+}
+
+// Add try-catch in level1
+//Observe stack trace change
