14b3441df7be6723bcc230f35bf0d4fa68f55c7d
Munna Aziz Mondal <azizmunna661@gmail.com>
2026-01-19 17:13:13 +0530

    Exception Propagation

---
+++ src/exceptionRevision/Example8.java
@@ -0,0 +1,21 @@
+package exceptionRevision;
+
+public class Example8 {
+
+    static void method1() {
+        int x = 10 / 0;
+    }
+
+    static void method2() {
+        method1();
+    }
+
+    public static void main(String[] args) {
+        try {
+            method2();
+        } catch (ArithmeticException e) {
+            System.out.println("Handled in main");
+        }
+    }
+}
+
