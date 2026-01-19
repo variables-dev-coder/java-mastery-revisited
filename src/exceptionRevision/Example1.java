cc6151ded3b354a28baaf7b1f64c362e6b1b5360
Munna Aziz Mondal <azizmunna661@gmail.com>
2026-01-19 16:51:38 +0530

    Basic try-catch (ArithmeticException)

---
+++ src/exceptionRevision/Example1.java
@@ -0,0 +1,12 @@
+package exceptionRevision;
+
+public class Example1 {
+    public static void main(String[] args) {
+        try {
+            int a = 10 / 0;
+        } catch (ArithmeticException e) {
+            System.out.println("Cannot divide by zero");
+        }
+    }
+}
+
