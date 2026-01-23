5690126891e320d559bb02c2fbce3ac912149c52
Munna Aziz Mondal <azizmunna661@gmail.com>
2026-01-23 18:19:05 +0530

    Sum of Array Elements

---
+++ src/revisionArray/SumArray.java
@@ -0,0 +1,12 @@
+package revisionArray;
+
+public class SumArray {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4};
+        int sum = 0;
+
+        for (int n : arr) sum += n;
+        System.out.println(sum);
+    }
+}
+
