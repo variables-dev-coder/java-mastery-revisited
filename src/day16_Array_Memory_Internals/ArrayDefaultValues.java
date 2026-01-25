e9ab4e6aaa9eb5224d560656923dab47d4aa8320
Munna Aziz Mondal <azizmunna661@gmail.com>
2026-01-25 18:23:28 +0530

    Array, tack vs Heap + Default Values

---
+++ src/day16_Array_Memory_Internals/ArrayDefaultValues.java
@@ -0,0 +1,13 @@
+package day16_Array_Memory_Internals;
+
+public class ArrayDefaultValues {
+    public static void main(String[] args) {
+
+        int[] nums = new int[3];
+
+        System.out.println(nums[0]); // 0
+        System.out.println(nums[1]); // 0
+        System.out.println(nums[2]); // 0
+    }
+}
+
