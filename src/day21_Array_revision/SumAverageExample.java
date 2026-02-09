d5421a21d4d446efbbb6b163f42eece44b6929e8
Munna Aziz Mondal <azizmunna661@gmail.com>
2026-02-09 17:09:39 +0530

    Array, Sum and Average of Array

---
+++ src/day21_Array_revision/SumAverageExample.java
@@ -0,0 +1,19 @@
+package day21_Array_revision;
+
+public class SumAverageExample {
+
+    public static void main(String[] args) {
+
+        int[] arr = {10, 20, 30, 40};
+        int sum = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+
+        double avg = (double) sum / arr.length;
+
+        System.out.println("Sum = " + sum);
+        System.out.println("Average = " + avg);
+    }
+}
