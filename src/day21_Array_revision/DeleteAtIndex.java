b6f45813c6301504b603c05051b3dfa09965fdc7
Munna Aziz Mondal <azizmunna661@gmail.com>
2026-02-09 17:14:54 +0530

    Array, Delete Element from Given Index

---
+++ src/day21_Array_revision/DeleteAtIndex.java
@@ -0,0 +1,21 @@
+package day21_Array_revision;
+
+public class DeleteAtIndex {
+
+    public static void main(String[] args) {
+
+        int[] arr = {10, 20, 30, 40, 50};
+        int size = arr.length;
+        int index = 1;
+
+        for (int i = index; i < size - 1; i++) {
+            arr[i] = arr[i + 1];
+        }
+
+        size--;
+
+        for (int i = 0; i < size; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
