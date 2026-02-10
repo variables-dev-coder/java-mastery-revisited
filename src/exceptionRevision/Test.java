5102dba2233b25a392a6ea1863fa7d43d3629a17
Munna Aziz Mondal <azizmunna661@gmail.com>
2026-02-10 22:38:18 +0530

    Custom Exception (Business Rule Violation)

---
+++ src/exceptionRevision/Test.java
@@ -0,0 +1,25 @@
+package exceptionRevision;
+
+class InvalidAgeException extends Exception {
+    public InvalidAgeException(String message) {
+        super(message);
+    }
+}
+
+class Test {
+    static void checkAge(int age) throws InvalidAgeException {
+        if (age < 18) {
+            throw new InvalidAgeException("Age must be 18 or above");
+        }
+        System.out.println("Eligible to vote");
+    }
+
+    public static void main(String[] args) {
+        try {
+            checkAge(16);
+        } catch (InvalidAgeException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
+
