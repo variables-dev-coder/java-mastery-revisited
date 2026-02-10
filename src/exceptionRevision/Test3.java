4c6a2de66ba3c64f2fe4b0e527742d77606d8e37
Munna Aziz Mondal <azizmunna661@gmail.com>
2026-02-10 22:44:00 +0530

    try-with-resources

---
+++ src/exceptionRevision/Test3.java
@@ -0,0 +1,16 @@
+package exceptionRevision;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Test3 {
+
+    public static void main(String[] args) {
+        try (BufferedReader br = new BufferedReader(new FileReader("data.txt"))) {
+            System.out.println(br.readLine());
+        } catch (IOException e) {
+            System.out.println("IO error occurred");
+        }
+    }
+}
