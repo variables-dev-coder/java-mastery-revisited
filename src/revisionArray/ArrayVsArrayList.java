27c9b990fb6a951d1ac9951af662c75377ff9797
Munna Aziz Mondal <azizmunna661@gmail.com>
2026-02-11 18:52:54 +0530

    Array vs ArrayList

---
+++ src/revisionArray/ArrayVsArrayList.java
@@ -0,0 +1,25 @@
+package revisionArray;
+
+import java.util.ArrayList;
+
+public class ArrayVsArrayList {
+
+    public static void main(String[] args) {
+
+        // Array
+        int[] arr = new int[3];
+        arr[0] = 10;
+        arr[1] = 20;
+        arr[2] = 30;
+
+        // ArrayList
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.add(40);
+
+        System.out.println(arr[1]);
+        System.out.println(list.get(1));
+    }
+}
