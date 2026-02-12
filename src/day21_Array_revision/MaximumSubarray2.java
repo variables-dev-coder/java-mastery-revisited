a8b89e31dfa0f1c7f6db14be968666ddded79873
Munna Aziz Mondal <azizmunna661@gmail.com>
2026-02-12 21:46:12 +0530

    Maximum Subarray Sum (Kadane’s Algorithm)

---
+++ src/day21_Array_revision/MaximumSubarray2.java
@@ -0,0 +1,20 @@
+package day21_Array_revision;
+
+public class MaximumSubarray2 {
+
+    public static void main(String[] args) {
+
+        int[] nums = {-2,1,-3,4,-1,2,1,-5,4};
+
+        int currentSum = nums[0];
+        int maxSum = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+
+            currentSum = Math.max(nums[i], currentSum + nums[i]);
+            maxSum = Math.max(maxSum, currentSum);
+        }
+
+        System.out.println("Maximum Subarray Sum = " + maxSum);
+    }
+}
