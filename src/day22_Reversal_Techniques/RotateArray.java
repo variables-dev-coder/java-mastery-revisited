7e3e5441649b2be7299a563767a4292c0b0b476a
Munna Aziz Mondal <azizmunna661@gmail.com>
2026-02-13 19:34:30 +0530

    Array, Rotate Array by K

---
+++ src/day22_Reversal_Techniques/RotateArray.java
@@ -0,0 +1,30 @@
+package day22_Reversal_Techniques;
+
+public class RotateArray {
+
+    public static void main(String[] args) {
+
+        int[] arr = {1, 2, 3, 4, 5, 6, 7};
+        int k = 3;
+        int n = arr.length;
+
+        k = k % n;
+
+        reverse(arr, 0, n - 1);
+        reverse(arr, 0, k - 1);
+        reverse(arr, k, n - 1);
+
+        for (int num : arr)
+            System.out.print(num + " ");
+    }
+
+    static void reverse(int[]arr, int left, int right) {
+        while (left < right) {
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left++;
+            right--;
+        }
+    }
+}
