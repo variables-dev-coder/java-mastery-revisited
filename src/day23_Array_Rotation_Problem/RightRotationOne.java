ee4e93831042059fc7056a38729d2ab31051ad35
Munna Aziz Mondal <azizmunna661@gmail.com>
2026-02-15 15:15:36 +0530

    array, Right Rotation example

--- src/day23_Array_Rotation_Problem/RightRotationOne.java
+++ src/day23_Array_Rotation_Problem/RightRotationOne.java
@@ -1,22 +1,20 @@
 package day23_Array_Rotation_Problem;
 
-public class LeftRotateOne {
+public class RightRotationOne {
 
     public static void main(String[] args) {
 
         int[] arr = {1, 2, 3, 4, 5};
 
-        int first = arr[0];
+        int last = arr[arr.length - 1];
 
-        for (int i = 0; i < arr.length - 1; i++) {
-            arr[i] = arr[i + 1];
+        for (int i = arr.length - 1; i > 0; i--) {
+            arr[i] = arr[i - 1];
         }
 
-        arr[arr.length - 1] = first;
+        arr[0] = last;
 
         for (int num : arr)
             System.out.print(num + " ");
     }
 }
-
-// 2 3 4 5 1
