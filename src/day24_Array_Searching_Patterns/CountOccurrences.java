d437f36dd587fb127554b007185554ffd7ef18e1
Munna Aziz Mondal <azizmunna661@gmail.com>
2026-02-17 19:42:14 +0530

    Array, Count Occurrence

---
+++ src/day24_Array_Searching_Patterns/CountOccurrences.java
@@ -0,0 +1,20 @@
+package day24_Array_Searching_Patterns;
+
+public class CountOccurrences {
+
+    public static void main(String[] args) {
+
+        int[] arr = {1, 2, 3, 2, 2, 5};
+        int key = 2;
+        int count = 0;
+
+        for (int num : arr) {
+            if (num == key){
+                count++;
+            }
+           
+        }
+
+        System.out.println("Occurrences: " + count);
+    }
+}
