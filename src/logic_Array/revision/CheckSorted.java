7e58cbb1e6e059d0bfee8c357333e2afc68bd308
Munna Aziz Mondal <azizmunna661@gmail.com>
2026-02-21 21:26:37 +0530

    Array, Check if Array is Sorted

---
+++ src/logic_Array/revision/CheckSorted.java
@@ -0,0 +1,18 @@
+package logic_Array.revision;
+
+public class CheckSorted {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+
+        boolean isSorted = true;
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < arr[i - 1]) {
+                isSorted = false;
+                break;
+            }
+        }
+
+        System.out.println("Is Sorted? " + isSorted);
+    }
+}
