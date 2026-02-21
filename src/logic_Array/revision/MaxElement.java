04aa86da9d7732704c9abd5908b18ed2cbbadc47
Munna Aziz Mondal <azizmunna661@gmail.com>
2026-02-21 21:24:41 +0530

    Array, Find Maximum Element

---
+++ src/logic_Array/revision/MaxElement.java
@@ -0,0 +1,17 @@
+package logic_Array.revision;
+
+public class MaxElement {
+    public static void main(String[] args) {
+        int[] arr = {5, 2, 9, 1, 7};
+
+        int max = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+
+        System.out.println("Max = " + max);
+    }
+}
