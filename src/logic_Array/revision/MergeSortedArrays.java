81981ebee74293f4de5698e4b88c3473ef18e4dd
Munna Aziz Mondal <azizmunna661@gmail.com>
2026-02-25 21:40:09 +0530

    Array, Merge Two Sorted Arrays

---
+++ src/logic_Array/revision/MergeSortedArrays.java
@@ -0,0 +1,37 @@
+package logic_Array.revision;
+
+public class MergeSortedArrays {
+
+    public static void main(String[] args) {
+
+        int[] arr1 = {1, 3, 5};
+        int[] arr2 = {2, 4, 6};
+
+        int n1 = arr1.length;
+        int n2 = arr2.length;
+
+        int[] merged = new int[n1 + n2];
+
+        int i = 0, j = 0, k = 0;
+
+        while (i < n1 && j < n2) {
+            if (arr1[i] < arr2[j]) {
+                merged[k++] = arr1[i++];
+            } else {
+                merged[k++] = arr2[j++];
+            }
+        }
+
+        while (i < n1) {
+            merged[k++] = arr1[i++];
+        }
+
+        while (j < n2) {
+            merged[k++] = arr2[j++];
+        }
+
+        for (int num : merged) {
+            System.out.print(num + " ");
+        }
+    }
+}
