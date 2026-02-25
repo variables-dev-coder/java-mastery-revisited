38b254f6b53d946f08b5740098183e5973a5530f
Munna Aziz Mondal <azizmunna661@gmail.com>
2026-02-25 21:43:37 +0530

    Array, Pair Sum Sorted Array

---
+++ src/logic_Array/revision/PairSumSorted.java
@@ -0,0 +1,29 @@
+package logic_Array.revision;
+
+public class PairSumSorted {
+
+    public static void main(String[] args) {
+
+        int[] arr = {1, 2, 3, 4, 6, 8};
+        int target = 10;
+
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left < right) {
+
+            int sum = arr[left] + arr[right];
+
+            if (sum == target) {
+                System.out.println("Pair found: " + arr[left] + " and " + arr[right]);
+                return;
+            } else if (sum < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        System.out.println("No pair found");
+    }
+}
