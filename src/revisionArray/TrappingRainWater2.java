b70cb55953559a127d3b3018dfd060f08f90798e
Munna Aziz Mondal <azizmunna661@gmail.com>
2026-02-26 21:58:36 +0530

    Array, Trapping Rain Water

---
+++ src/revisionArray/TrappingRainWater2.java
@@ -0,0 +1,48 @@
+package revisionArray;
+
+public class TrappingRainWater2 {
+
+    public static int trap(int[] height) {
+
+        int left = 0;
+        int right = height.length - 1;
+
+        int leftMax = 0;
+        int rightMax = 0;
+
+        int water = 0;
+
+        while (left < right) {
+
+            if (height[left] < height[right]) {
+
+                if (height[left] >= leftMax)
+                    leftMax = height[left];
+                else
+                    water += leftMax - height[left];
+
+                left++;
+
+            } else {
+
+                if (height[right] >= rightMax)
+                    rightMax = height[right];
+                else
+                    water += rightMax - height[right];
+
+                right--;
+            }
+        }
+
+        return water;
+    }
+
+    public static void main(String[] args) {
+
+        int[] height = {0,1,0,2,1,0,1,3,2,1,2,1};
+
+        int result = trap(height);
+
+        System.out.println("Trapped Water = " + result);
+    }
+}
