8d2a7869ab80daf67c5020c10b2769453101abc6
Munna Aziz Mondal <azizmunna661@gmail.com>
2026-03-02 17:12:06 +0530

    Array Reverse example

---
+++ src/revisionArray/ArrayReverse.java
@@ -0,0 +1,25 @@
+package revisionArray;
+
+public class ArrayReverse {
+
+    public static void main(String[] args) {
+
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left < right) {
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+
+            left++;
+            right--;
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
