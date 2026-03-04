f77fe0e516ea874f2ebd0da65dc8ffea67947c29
Munna Aziz Mondal <azizmunna661@gmail.com>
2026-03-04 21:50:39 +0530

    Trapping Rain Water

---
+++ src/revision2/TrappingRainWater.java
@@ -0,0 +1,41 @@
+package revision2;
+
+public class TrappingRainWater {
+
+    public static int trap(int[] height) {
+
+        int left = 0, right = height.length - 1;
+        int leftMax = 0, rightMax = 0;
+        int water = 0;
+
+        while (left < right) {
+
+            if (height[left] < height[right]) {
+
+                if (height[left] >= leftMax)
+                    leftMax = height[left];
+                else
+                    water += leftMax - height[left];
+
+                left++;
+
+            } else {
+
+                if (height[right] >= rightMax)
+                    rightMax = height[right];
+                else
+                    water += rightMax - height[right];
+
+                right--;
+            }
+        }
+
+        return water;
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {4,2,0,3,2,5};
+        System.out.println(trap(arr)); // Output: 9
+    }
+}
