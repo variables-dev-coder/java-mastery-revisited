77d99f7d101a11792b228057eee574a34bcdc7c6
Munna Aziz Mondal <azizmunna661@gmail.com>
2026-03-04 22:03:58 +0530

    Container With Most Water

---
+++ src/revision2/ContainerWithMostWater.java
@@ -0,0 +1,32 @@
+package revision2;
+
+public class ContainerWithMostWater {
+
+    public static int maxArea(int[] height) {
+
+        int left = 0;
+        int right = height.length - 1;
+        int maxArea = 0;
+
+        while (left < right) {
+
+            int area = Math.min(height[left], height[right]) * (right - left);
+
+            maxArea = Math.max(maxArea, area);
+
+            if (height[left] < height[right])
+                left++;
+            else
+                right--;
+        }
+
+        return maxArea;
+    }
+
+    public static void main(String[] args) {
+
+        int[] height = {1,8,6,2,5,4,8,3,7};
+
+        System.out.println(maxArea(height)); // Output: 49
+    }
+}
