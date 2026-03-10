762f4ce70a94da5697079c5f7e0dd661c73838f9
Munna Aziz Mondal <azizmunna661@gmail.com>
2026-03-10 19:02:48 +0530

    Minimum Size Subarray Sum

---
+++ src/logic_Array/day10_Fixed_Window_Sum/MinimumSubarraySum.java
@@ -0,0 +1,32 @@
+package logic_Array.day10_Fixed_Window_Sum;
+
+public class MinimumSubarraySum {
+
+    public static void main(String[] args) {
+
+        int[] nums = {2,3,1,2,4,3};
+        int target = 7;
+
+        int left = 0;
+        int sum = 0;
+        int minLength = Integer.MAX_VALUE;
+
+        for (int right = 0; right < nums.length; right++) {
+
+            sum += nums[right];
+
+            while (sum >= target) {
+
+                minLength = Math.min(minLength, right - left + 1);
+
+                sum -= nums[left];
+                left++;
+            }
+        }
+
+        if (minLength == Integer.MAX_VALUE)
+            System.out.println(0);
+        else
+            System.out.println("Minimum Length = " + minLength);
+    }
+}
