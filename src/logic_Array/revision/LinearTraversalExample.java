56886f99226df20b05b744095d34d2a9926914b9
Munna Aziz Mondal <azizmunna661@gmail.com>
2026-03-17 22:06:33 +0530

    Basic Linear Traversal

---
+++ src/logic_Array/revision/LinearTraversalExample.java
@@ -0,0 +1,15 @@
+package logic_Array.revision;
+
+public class LinearTraversalExample {
+
+    public static void main(String[] args) {
+
+        int[] arr = {5, 3, 8, 4, 2};
+
+        System.out.println("Array elements:");
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
