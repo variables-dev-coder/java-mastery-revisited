b25351762ea298a6a0e8f2c05bdfef4e301b5595
Munna Aziz Mondal <azizmunna661@gmail.com>
2026-03-17 22:20:44 +0530

    update commit

--- src/logic_Array/revision/ReverseArray3.java
+++ src/logic_Array/revision/ReverseArray3.java
@@ -1,4 +1,35 @@
 package logic_Array.revision;
 
 public class ReverseArray3 {
+
+    public static void reverse(int[] arr) {
+
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start < end) {
+
+            // swap
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+
+            // move pointers
+            start++;
+            end--;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {5, 3, 8, 4, 2};
+
+        reverse(arr);
+
+        System.out.print("Reversed Array: ");
+
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
 }
