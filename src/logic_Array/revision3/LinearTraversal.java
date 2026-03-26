e0d41fbec90586d549db2223e263eff36272bdb8
Munna Aziz Mondal <azizmunna661@gmail.com>
2026-03-26 20:11:17 +0530

    Array Linear Traversal

---
+++ src/logic_Array/revision3/LinearTraversal.java
@@ -0,0 +1,14 @@
+package logic_Array.revision3;
+
+public class LinearTraversal {
+
+    public static void main(String[] args) {
+
+        int[] arr = {10, 20, 30, 40, 50};
+
+        // Linear traversal
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
---
+++ src/logic_Array/revision3/LinearTraversal2.java
@@ -0,0 +1,13 @@
+package logic_Array.revision3;
+
+public class LinearTraversal2 {
+
+    public static void main(String[] args) {
+
+        int[] arr = {10, 20, 30, 40, 50};
+
+        for (int num : arr) {
+            System.out.println(num);
+        }
+    }
+}
