7ad7390d4a3ee3f308ea350362c181ea852a15a1
Munna Aziz Mondal <azizmunna661@gmail.com>
2026-04-07 17:49:51 +0530

    revision, Prefix Sum

---
+++ src/logic_Array/revision_1_15_days/Day12_PrefixSum.java
@@ -0,0 +1,32 @@
+package logic_Array.revision_1_15_days;
+
+public class Day12_PrefixSum {
+
+    public static void main(String[] args) {
+
+        int[] nums = {3, 1, 4, 2, 5};
+
+        int n = nums.length;
+        int[] prefix = new int[n];
+
+        // build prefix array
+        prefix[0] = nums[0];
+
+        for (int i = 1; i < n; i++) {
+            prefix[i] = prefix[i - 1] + nums[i];
+        }
+
+        // query
+        int L = 1;
+        int R = 3;
+
+        int sum;
+
+        if (L == 0)
+            sum = prefix[R];
+        else
+            sum = prefix[R] - prefix[L - 1];
+
+        System.out.println("Range Sum = " + sum);
+    }
+}
