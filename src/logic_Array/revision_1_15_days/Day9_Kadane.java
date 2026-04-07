c018f6df13d03d0157648ae56fbdc8db4de2cb78
Munna Aziz Mondal <azizmunna661@gmail.com>
2026-04-07 17:42:15 +0530

    revision, Maximum Subarray Sum

---
+++ src/logic_Array/revision_1_15_days/Day9_Kadane.java
@@ -0,0 +1,20 @@
+package logic_Array.revision_1_15_days;
+
+public class Day9_Kadane {
+
+    public static void main(String[] args) {
+
+        int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+
+        int currentSum = nums[0];
+        int maxSum = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+
+            currentSum = Math.max(nums[i], currentSum + nums[i]);
+            maxSum = Math.max(maxSum, currentSum);
+        }
+
+        System.out.println("Maximum Subarray Sum = " + maxSum);
+    }
+}
