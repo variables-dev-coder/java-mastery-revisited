2b7470865875a5bc018c6770550e2caa8aa1e098
Munna Aziz Mondal <azizmunna661@gmail.com>
2026-04-10 18:55:18 +0530

    Median of Two Sorted Arrays

---
+++ src/revision2/MedianOfTwoSortedArrays.java
@@ -0,0 +1,59 @@
+package revision2;
+
+public class MedianOfTwoSortedArrays {
+
+    public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
+
+        // Ensure nums1 is smaller
+        if (nums1.length > nums2.length) {
+            return findMedianSortedArrays(nums2, nums1);
+        }
+
+        int n1 = nums1.length;
+        int n2 = nums2.length;
+
+        int low = 0, high = n1;
+
+        while (low <= high) {
+
+            int cut1 = (low + high) / 2;
+            int cut2 = (n1 + n2 + 1) / 2 - cut1;
+
+            int l1 = (cut1 == 0) ? Integer.MIN_VALUE : nums1[cut1 - 1];
+            int l2 = (cut2 == 0) ? Integer.MIN_VALUE : nums2[cut2 - 1];
+
+            int r1 = (cut1 == n1) ? Integer.MAX_VALUE : nums1[cut1];
+            int r2 = (cut2 == n2) ? Integer.MAX_VALUE : nums2[cut2];
+
+            // correct partition
+            if (l1 <= r2 && l2 <= r1) {
+
+                if ((n1 + n2) % 2 == 0) {
+                    return (Math.max(l1, l2) + Math.min(r1, r2)) / 2.0;
+                } else {
+                    return Math.max(l1, l2);
+                }
+            }
+            // move left
+            else if (l1 > r2) {
+                high = cut1 - 1;
+            }
+            // move right
+            else {
+                low = cut1 + 1;
+            }
+        }
+
+        return 0.0;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums1 = {1, 3};
+        int[] nums2 = {2};
+
+        double result = findMedianSortedArrays(nums1, nums2);
+
+        System.out.println("Median: " + result);
+    }
+}
