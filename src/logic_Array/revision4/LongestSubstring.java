75c80168ba30f412f0b3320f0dfb0caf82450a58
Munna Aziz Mondal <azizmunna661@gmail.com>
2026-05-04 09:52:57 +0530

    Added Longest Substring Without Repeating Characters

---
+++ src/logic_Array/revision4/LongestSubstring.java
@@ -0,0 +1,25 @@
+package logic_Array.revision4;
+
+import java.util.HashSet;
+
+public class LongestSubstring {
+    public static void main(String[] args) {
+        String s = "abcabcbb";
+
+        HashSet<Character> set = new HashSet<>();
+        int left = 0, maxLength = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+
+            while (set.contains(s.charAt(right))) {
+                set.remove(s.charAt(left));
+                left++;
+            }
+
+            set.add(s.charAt(right));
+            maxLength = Math.max(maxLength, right - left + 1);
+        }
+
+        System.out.println("Longest Length: " + maxLength);
+    }
+}
