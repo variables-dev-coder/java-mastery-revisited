9fc8c11c38b80b9e4f0f8ffeb1cc2dcd99ce7d29
Munna Aziz Mondal <azizmunna661@gmail.com>
2026-05-04 09:39:27 +0530

    Added Maximum Subarray Sum (Kadane’s Algorithm)

---
+++ src/logic_Array/revision4/MaxSubarray.java
@@ -0,0 +1,17 @@
+package logic_Array.revision4;
+
+public class MaxSubarray {
+    public static void main(String[] args) {
+        int[] arr = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+
+        int maxSum = arr[0];
+        int currentSum = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            currentSum = Math.max(arr[i], currentSum + arr[i]);
+            maxSum = Math.max(maxSum, currentSum);
+        }
+
+        System.out.println("Maximum Subarray Sum: " + maxSum);
+    }
+}
