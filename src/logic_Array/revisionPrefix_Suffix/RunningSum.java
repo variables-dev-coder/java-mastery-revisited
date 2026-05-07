042c799a4003cb681b83ac51715ff877726dc0a3
Munna Aziz Mondal <azizmunna661@gmail.com>
2026-05-07 15:43:30 +0530

    Added Prefix and Suffix Running Sum of Array

---
+++ src/logic_Array/revisionPrefix_Suffix/RunningSum.java
@@ -0,0 +1,18 @@
+package logic_Array.revisionPrefix_Suffix;
+
+public class RunningSum {
+
+    public static void main(String[] args) {
+
+        int[] nums = {1,2,3,4};
+
+        for (int i = 1; i < nums.length; i++) {
+
+            nums[i] = nums[i] + nums[i - 1];
+        }
+
+        for (int num : nums) {
+            System.out.print(num + " ");
+        }
+    }
+}
