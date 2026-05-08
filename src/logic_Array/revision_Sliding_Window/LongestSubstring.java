127256ac02f6de5b71344eb19dce3ceba97a4e70
Munna Aziz Mondal <azizmunna661@gmail.com>
2026-05-08 17:24:49 +0530

    Added Sliding Window Longest Substring Without Repeating Characters

---
+++ src/logic_Array/revision_Sliding_Window/LongestSubstring.java
@@ -0,0 +1,38 @@
+package logic_Array.revision_Sliding_Window;
+
+import java.util.HashSet;
+
+public class LongestSubstring {
+
+    public static void main(String[] args) {
+
+        String s = "abcabcbb";
+
+        HashSet<Character> set = new HashSet<>();
+
+        int left = 0;
+
+        int maxLength = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+
+            // Remove duplicates
+            while (set.contains(s.charAt(right))) {
+
+                set.remove(s.charAt(left));
+
+                left++;
+            }
+
+            // Add current character
+            set.add(s.charAt(right));
+
+            // Update maximum length
+            maxLength =
+                    Math.max(maxLength,
+                            right - left + 1);
+        }
+
+        System.out.println("Longest Length: " + maxLength);
+    }
+}
