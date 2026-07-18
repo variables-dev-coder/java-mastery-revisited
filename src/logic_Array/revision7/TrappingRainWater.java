1b040ceee6d58e0a75ae9ac7d064ea47fc231ef3
Munna Aziz Mondal <azizmunna661@gmail.com>
2026-07-18 17:19:37 +0530

    LC 42 - Trapping Rain Water

---
+++ src/logic_Array/revision7/TrappingRainWater.java
@@ -0,0 +1,48 @@
+package logic_Array.revision7;
+
+public class TrappingRainWater {
+
+    public static int trap(int[] height) {
+
+        int left = 0;
+        int right = height.length - 1;
+
+        int leftMax = 0;
+        int rightMax = 0;
+
+        int water = 0;
+
+        while (left < right) {
+
+            if (height[left] < height[right]) {
+
+                if (height[left] >= leftMax) {
+                    leftMax = height[left];
+                } else {
+                    water += leftMax - height[left];
+                }
+
+                left++;
+
+            } else {
+
+                if (height[right] >= rightMax) {
+                    rightMax = height[right];
+                } else {
+                    water += rightMax - height[right];
+                }
+
+                right--;
+            }
+        }
+
+        return water;
+    }
+
+    public static void main(String[] args) {
+
+        int[] height = {4,2,0,3,2,5};
+
+        System.out.println(trap(height));
+    }
+}
