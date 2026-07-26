d1c1831912b59bdbaf5dec6934f0df5da7289c66
Munna Aziz Mondal <azizmunna661@gmail.com>
2026-07-26 11:44:37 +0530

    Added Reverse String Problem

---
+++ src/revision3/ReverseString.java
@@ -0,0 +1,15 @@
+package revision3;
+
+public class ReverseString {
+    public static void main(String[] args) {
+        String str = "Hello";
+        String reverse = "";
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reverse += str.charAt(i);
+        }
+
+        System.out.println("Original : " + str);
+        System.out.println("Reversed : " + reverse);
+    }
+}
