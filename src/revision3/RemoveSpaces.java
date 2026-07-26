81069fc259963d234f80fc387f500cc94a65086f
Munna Aziz Mondal <azizmunna661@gmail.com>
2026-07-26 11:53:31 +0530

    Added Remove All Spaces problem

---
+++ src/revision3/RemoveSpaces.java
@@ -0,0 +1,12 @@
+package revision3;
+
+public class RemoveSpaces {
+    public static void main(String[] args) {
+
+        String str = "Java is Awesome";
+
+        String result = str.replace(" ", "");
+
+        System.out.println(result);
+    }
+}
