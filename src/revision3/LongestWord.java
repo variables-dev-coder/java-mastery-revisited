026ccf310f0f423ff437883eb6d9e10986017f20
Munna Aziz Mondal <azizmunna661@gmail.com>
2026-08-03 17:23:36 +0530

    Added Longest Word in a Sentence

---
+++ src/revision3/LongestWord.java
@@ -0,0 +1,20 @@
+package revision3;
+
+public class LongestWord {
+    public static void main(String[] args) {
+
+        String sentence = "Java is a powerful programming language";
+
+        String[] words = sentence.split(" ");
+
+        String longest = "";
+
+        for (String word : words) {
+            if (word.length() > longest.length()) {
+                longest = word;
+            }
+        }
+
+        System.out.println("Longest Word: " + longest);
+    }
+}
