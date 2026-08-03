4eea981ae44687aa058ab874146d52837e9c4788
Munna Aziz Mondal <azizmunna661@gmail.com>
2026-08-03 17:33:57 +0530

    Added String Length & String Upper Case example

---
+++ src/revision3/StringLengthExample.java
@@ -0,0 +1,14 @@
+package revision3;
+
+public class StringLengthExample {
+
+    public static void main(String[] args) {
+
+        String str = "Java Programming";
+
+        int length = str.length();
+
+        System.out.println("String : " + str);
+        System.out.println("Length : " + length);
+    }
+}
---
+++ src/revision3/UpperCaseExample.java
@@ -0,0 +1,14 @@
+package revision3;
+
+public class UpperCaseExample {
+
+    public static void main(String[] args) {
+
+        String str = "java programming";
+
+        String upper = str.toUpperCase();
+
+        System.out.println("Original String : " + str);
+        System.out.println("Uppercase String: " + upper);
+    }
+}
