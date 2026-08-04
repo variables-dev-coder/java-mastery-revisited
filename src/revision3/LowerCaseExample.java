4bbed7b730c9a736ae83c35d66dcccb8639b730e
Munna Aziz Mondal <azizmunna661@gmail.com>
2026-08-04 17:29:43 +0530

    Added Conver String Upper Case to Lower Case example

---
+++ src/revision3/LowerCaseExample.java
@@ -0,0 +1,14 @@
+package revision3;
+
+public class LowerCaseExample {
+
+    public static void main(String[] args) {
+
+        String str = "JAVA PROGRAMMING";
+
+        String lower = str.toLowerCase();
+
+        System.out.println("Original String : " + str);
+        System.out.println("Lowercase String: " + lower);
+    }
+}
