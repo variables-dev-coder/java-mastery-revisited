8eac0b5e4c4358e7ebbb22b86961ef55aaf7c653
Munna Aziz Mondal <azizmunna661@gmail.com>
2026-08-05 12:33:58 +0530

    Added Empty String

---
+++ src/revision3/IsEmptyExample.java
@@ -0,0 +1,11 @@
+package revision3;
+
+public class IsEmptyExample {
+
+    public static void main(String[] args) {
+
+        String str = "";
+
+        System.out.println(str.isEmpty());
+    }
+}
---
+++ src/revision3/IsEmptyExample2.java
@@ -0,0 +1,11 @@
+package revision3;
+
+public class IsEmptyExample2 {
+
+    public static void main(String[] args) {
+
+        String str = "Java";
+
+        System.out.println(str.isEmpty());
+    }
+}
