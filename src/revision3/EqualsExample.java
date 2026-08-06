ddf098c95be7e277806cc894534c88fb26b27da0
Munna Aziz Mondal <azizmunna661@gmail.com>
2026-08-06 16:29:04 +0530

    Added Two String comper example

---
+++ src/revision3/EqualsExample.java
@@ -0,0 +1,12 @@
+package revision3;
+
+public class EqualsExample {
+
+    public static void main(String[] args) {
+
+        String str1 = "Java";
+        String str2 = "Java";
+
+        System.out.println(str1.equals(str2));
+    }
+}
