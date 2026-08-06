2139f67e6d54ee2b6221461aab7dc9c795d13ba0
Munna Aziz Mondal <azizmunna661@gmail.com>
2026-08-06 16:31:51 +0530

    Added Compare Two String case-sensitive

---
+++ src/revision3/CompareStrings.java
@@ -0,0 +1,16 @@
+package revision3;
+
+public class CompareStrings {
+
+    public static void main(String[] args) {
+
+        String str1 = "Java";
+        String str2 = "Java";
+
+        if (str1.equals(str2)) {
+            System.out.println("Strings are Equal");
+        } else {
+            System.out.println("Strings are Not Equal");
+        }
+    }
+}
