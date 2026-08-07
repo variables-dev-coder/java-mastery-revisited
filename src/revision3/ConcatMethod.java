065b3aba1e5122698863dc3e1247b400684d5653
Munna Aziz Mondal <azizmunna661@gmail.com>
2026-08-07 14:58:32 +0530

    Added String Concatination Using concat()

---
+++ src/revision3/ConcatMethod.java
@@ -0,0 +1,15 @@
+package revision3;
+
+public class ConcatMethod {
+
+    public static void main(String[] args) {
+
+        String s1 = "Java";
+
+        String s2 = "Programming";
+
+        String result = s1.concat(s2);
+
+        System.out.println(result);
+    }
+}
