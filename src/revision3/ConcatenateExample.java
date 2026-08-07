bc516924145ddc2b652b531bb10593db10b4a901
Munna Aziz Mondal <azizmunna661@gmail.com>
2026-08-07 14:54:18 +0530

    Added String Concatenate Using + Opereator

---
+++ src/revision3/ConcatenateExample.java
@@ -0,0 +1,14 @@
+package revision3;
+
+public class ConcatenateExample {
+
+    public static void main(String[] args) {
+
+        String first = "Java";
+        String second = "Programming";
+
+        String result = first + second;
+
+        System.out.println(result);
+    }
+}
