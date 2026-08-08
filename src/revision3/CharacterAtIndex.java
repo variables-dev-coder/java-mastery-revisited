13078f3e7985f5553765fe8af7b26635a50eb95e
Munna Aziz Mondal <azizmunna661@gmail.com>
2026-08-08 15:54:13 +0530

    Added Finding Character at Given Index use charAt() Method

---
+++ src/revision3/CharacterAtIndex.java
@@ -0,0 +1,17 @@
+package revision3;
+
+public class CharacterAtIndex {
+
+    public static void main(String[] args) {
+
+        String str = "Programming";
+
+        int index = 4;
+
+        char ch = str.charAt(index);
+
+        System.out.println("String     : " + str);
+        System.out.println("Index      : " + index);
+        System.out.println("Character  : " + ch);
+    }
+}
