8d374145cf162c9d7b97477a3501ba3bf03529bd
Munna Aziz Mondal <azizmunna661@gmail.com>
2026-08-08 16:09:57 +0530

    Added Print Characters in Reverse

---
+++ src/revision3/ReverseString2.java
@@ -0,0 +1,14 @@
+package revision3;
+
+public class ReverseString2 {
+
+    public static void main(String[] args) {
+
+        String str = "Java";
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+
+            System.out.print(str.charAt(i));
+        }
+    }
+}
