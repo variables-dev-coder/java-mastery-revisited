e7b84a20942557ba90db58d1fe4c3cfa7f12109b
Munna Aziz Mondal <azizmunna661@gmail.com>
2026-08-09 16:06:04 +0530

    Added Count Total Characters in a String using length() method

---
+++ src/revision3/CountCharacters.java
@@ -0,0 +1,13 @@
+package revision3;
+
+public class CountCharacters {
+
+    public static void main(String[] args) {
+
+        String str = "Hello World";
+
+        int count = str.length();
+
+        System.out.println("Total Characters: " + count);
+    }
+}
