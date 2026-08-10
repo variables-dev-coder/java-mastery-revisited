1281e290c3c7f8a661b65891621256d2d09d7525
Munna Aziz Mondal <azizmunna661@gmail.com>
2026-08-10 11:37:36 +0530

    Added String Trim example

---
+++ src/revision3/TrimExample.java
@@ -0,0 +1,14 @@
+package revision3;
+
+public class TrimExample {
+
+    public static void main(String[] args) {
+
+        String str = "   Java Programming   ";
+
+        String result = str.trim();
+
+        System.out.println("Original : [" + str + "]");
+        System.out.println("Trimmed  : [" + result + "]");
+    }
+}
