8ea6ce22072f142bd07b225c721b374c2985a0cd
Munna Aziz Mondal <azizmunna661@gmail.com>
2026-08-11 15:09:03 +0530

    Inside String Replace Multiple Occurrences

---
+++ src/revision3/ReplaceStringMulti.java
@@ -0,0 +1,15 @@
+package revision3;
+
+public class ReplaceStringMulti {
+
+    public static void main(String[] args) {
+
+        String str = "Java is Java and Java is Powerfull";
+
+        String result = str.replace("Java", "Phython");
+
+        System.out.println("Original String is : " + str);
+
+        System.out.println("Updated String is : " + result);
+    }
+}
