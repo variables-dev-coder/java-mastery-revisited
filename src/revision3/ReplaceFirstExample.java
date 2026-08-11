4eb56676d4f47f448ac3d8d4053b40aac1ebf9a2
Munna Aziz Mondal <azizmunna661@gmail.com>
2026-08-11 15:19:47 +0530

    String replaceFirst example

---
+++ src/revision3/ReplaceFirstExample.java
@@ -0,0 +1,13 @@
+package revision3;
+
+public class ReplaceFirstExample {
+
+    public static void main(String[] args) {
+
+        String str = "Java Java Java";
+
+        String result = str.replaceFirst("Java", "Python");
+
+        System.out.println(result);
+    }
+}
