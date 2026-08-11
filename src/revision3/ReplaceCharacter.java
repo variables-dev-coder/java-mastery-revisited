12334f5f03922b4e5dc2435685f3c3f3e8096b7d
Munna Aziz Mondal <azizmunna661@gmail.com>
2026-08-11 14:58:23 +0530

    String, replace a particular character or part of the String with another value

---
+++ src/revision3/ReplaceCharacter.java
@@ -0,0 +1,14 @@
+package revision3;
+
+public class ReplaceCharacter {
+
+    public static void main(String[] args) {
+
+        String str = "banana";
+
+        String result = str.replace('a', 'o');
+
+        System.out.println("Original : " + str);
+        System.out.println("Result   : " + result);
+    }
+}
