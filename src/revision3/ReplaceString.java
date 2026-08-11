19eb29d1fb9e08adda986e903cc41044adda030d
Munna Aziz Mondal <azizmunna661@gmail.com>
2026-08-11 15:01:31 +0530

    Inside String replace a whole String/sequence, not just a single character

---
+++ src/revision3/ReplaceString.java
@@ -0,0 +1,14 @@
+package revision3;
+
+public class ReplaceString {
+
+    public static void main(String[] args) {
+
+        String str = "I love Java";
+
+        String result = str.replace("Java", "Python");
+
+        System.out.println("Original : " + str);
+        System.out.println("Result   : " + result);
+    }
+}
