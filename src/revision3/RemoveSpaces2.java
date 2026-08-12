9a95caf13d65bd61a31623310046fbfadc0d8aef
Munna Aziz Mondal <azizmunna661@gmail.com>
2026-08-12 09:01:59 +0530

    String, Remove All Spaces

---
+++ src/revision3/RemoveSpaces2.java
@@ -0,0 +1,13 @@
+package revision3;
+
+public class RemoveSpaces2 {
+
+    public static void main(String[] args) {
+
+        String str = "Java is a powerful language";
+
+        String result = str.replace(" ", "");
+
+        System.out.println(result);
+    }
+}
