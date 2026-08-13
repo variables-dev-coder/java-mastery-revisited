ec4904c8634e4fa3b211a14fa9affb82b2c9d5a5
Munna Aziz Mondal <azizmunna661@gmail.com>
2026-08-13 15:25:38 +0530

    String, Case-Sensitive Counting

---
+++ src/revision3/CaseSensitiveCount.java
@@ -0,0 +1,21 @@
+package revision3;
+
+public class CaseSensitiveCount {
+
+    public static void main(String[] args) {
+
+        String str = "Java JAVA java";
+        char target = 'a';
+
+        int count = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+
+            if (str.charAt(i) == target) {
+                count++;
+            }
+        }
+
+        System.out.println("Count: " + count);
+    }
+}
