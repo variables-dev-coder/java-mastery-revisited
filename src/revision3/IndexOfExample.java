b462e2bfc44612ca264bb9ec15b606335f1994d7
Munna Aziz Mondal <azizmunna661@gmail.com>
2026-08-14 16:31:51 +0530

    String, Using indexOf()

---
+++ src/revision3/IndexOfExample.java
@@ -0,0 +1,13 @@
+package revision3;
+
+public class IndexOfExample {
+
+    public static void main(String[] args) {
+
+        String str = "programming";
+
+        int index = str.indexOf('g');
+
+        System.out.println(index);
+    }
+}
