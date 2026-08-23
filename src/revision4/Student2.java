0dc79142a4c7be8856a8439139cd147f4aa5ad5f
Munna Aziz Mondal <azizmunna661@gmail.com>
2026-08-23 15:43:11 +0530

    Instance Methods and Object State

---
+++ src/revision4/Student2.java
@@ -0,0 +1,12 @@
+package revision4;
+
+public class Student2 {
+
+    String name;
+    int age;
+
+    void display2() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+    }
+}
---
+++ src/revision4/Student2Main.java
@@ -0,0 +1,14 @@
+package revision4;
+
+public class Student2Main {
+
+    public static void main(String[] args) {
+
+        Student2 student2 = new Student2();
+
+        student2.name = "Munna";
+        student2.age = 31;
+
+        student2.display2();
+    }
+}
