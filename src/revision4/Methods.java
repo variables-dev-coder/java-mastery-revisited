cdabe9de2a11368f6946217a3aeafcfcc6d3a6bb
Munna Aziz Mondal <azizmunna661@gmail.com>
2026-08-25 14:46:51 +0530

    Method with No Parameter and No Return

---
+++ src/revision4/Methods.java
@@ -0,0 +1,14 @@
+package revision4;
+
+public class Methods {
+
+    static void greet() {
+        System.out.println("Hello, Java!");
+    }
+
+    public static void main(String[] args) {
+
+        greet();
+        greet();
+    }
+}
