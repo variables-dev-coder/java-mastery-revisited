0f7b5956a19e68e6e6ec0a9d1ce5565fae7bfc40
Munna Aziz Mondal <azizmunna661@gmail.com>
2026-08-25 14:48:48 +0530

    Method with Parameters

---
+++ src/revision4/ParameterMethods.java
@@ -0,0 +1,16 @@
+package revision4;
+
+public class ParameterMethods {
+
+    static void greet(String name, int age) {
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+    }
+
+    public static void main(String[] args) {
+
+        greet("Munna", 31);
+        greet("Rahul", 25);
+    }
+}
