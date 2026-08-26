550953a6e21bc251ad3d43dd057b3541c3edfcd2
Munna Aziz Mondal <azizmunna661@gmail.com>
2026-08-26 15:46:55 +0530

    revision, Stack example

---
+++ src/revision_stack_heap/StudentMain.java
@@ -0,0 +1,19 @@
+package revision_stack_heap;
+
+class Student {
+    int age;
+    String name;
+}
+
+public class StudentMain {
+
+    public static void main(String[] args) {
+
+        int x = 10;
+
+        Student s = new Student();
+
+        s.age = 20;
+        s.name = "Aziz";
+    }
+}
