f9e29f0976cb961e701f43f0ed6a6225bfd767c2
Munna Aziz Mondal <azizmunna661@gmail.com>
2026-08-26 15:52:11 +0530

    Array and Heap

---
+++ src/revision_stack_heap/Main.java
@@ -0,0 +1,15 @@
+package revision_stack_heap;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[] numbers = new int[3];
+
+        numbers[0] = 10;
+        numbers[1] = 20;
+        numbers[2] = 30;
+
+        System.out.println(numbers[1]);
+    }
+}
