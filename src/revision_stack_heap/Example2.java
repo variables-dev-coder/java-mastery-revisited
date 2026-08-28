0e5dbeaf0c61b788709daa5b1cf73cc30f361952
Munna Aziz Mondal <azizmunna661@gmail.com>
2026-08-28 18:04:08 +0530

    Added, Object in Heap + Reference

---
+++ src/revision_stack_heap/Example2.java
@@ -0,0 +1,21 @@
+package revision_stack_heap;
+
+class Student2 {
+
+    int age;
+    String name;
+}
+
+public class Example2 {
+
+    public static void main(String[] args) {
+
+        Student2 s = new Student2();
+
+        s.age = 25;
+        s.name = "Aziz";
+
+        System.out.println("Name: " + s.name);
+        System.out.println("Age: " + s.age);
+    }
+}
