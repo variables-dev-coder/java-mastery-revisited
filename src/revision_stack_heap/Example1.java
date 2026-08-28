dc9b2260dff6c730215514a2bd9dcfd1179f7302
Munna Aziz Mondal <azizmunna661@gmail.com>
2026-08-28 18:00:29 +0530

    Added Stack and Local Variable example

---
+++ src/revision_stack_heap/Example1.java
@@ -0,0 +1,20 @@
+package revision_stack_heap;
+
+public class Example1 {
+
+    static void calculate() {
+
+        int a = 10;
+        int b = 20;
+        int sum = a + b;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("sum = " + sum);
+    }
+
+    public static void main(String[] args) {
+
+        calculate();
+    }
+}
