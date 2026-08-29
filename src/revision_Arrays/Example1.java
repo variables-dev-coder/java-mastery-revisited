2503afa2d76923c1c183ca1ec0e07f717b9d5787
Munna Aziz Mondal <azizmunna661@gmail.com>
2026-08-29 18:53:07 +0530

    Revision Arrays

---
+++ src/revision_Arrays/Example1.java
@@ -0,0 +1,23 @@
+package revision_Arrays;
+
+import java.util.Scanner;
+
+public class Example1 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < n; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
