0ad346369dd373e98fe617b83178d85f8163b27c
Munna Aziz Mondal <azizmunna661@gmail.com>
2026-08-31 19:47:34 +0530

    Revised, Arrays Passing arrays to methods

---
+++ src/revision_Arrays/Example8.java
@@ -0,0 +1,19 @@
+package revision_Arrays;
+
+public class Example8 {
+
+    static void printArray(int[] arr) {
+
+        for (int x : arr) {
+            System.out.println(x);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int[] numbers = {10, 20, 30};
+
+        printArray(numbers);
+
+    }
+}
