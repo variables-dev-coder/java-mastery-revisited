1ca6a1785b7a190c3d890ac8569216b9e7f3c502
Munna Aziz Mondal <azizmunna661@gmail.com>
2026-08-31 15:51:07 +0530

    Revised, ArrayList Example

---
+++ src/revision_Arrays/Example4.java
@@ -0,0 +1,28 @@
+package revision_Arrays;
+
+import java.util.ArrayList;
+
+public class Example4 {
+
+    public static void main(String[] args) {
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.add(40);
+        list.add(50);
+
+        System.out.println(list);
+    }
+}
+
+
+/*
+int[] arr = new int[5]; -> int[] arr = new int[5];
+
+ArrayList<Integer> list = new ArrayList<>(); -> Resizable
+
+
+ */
