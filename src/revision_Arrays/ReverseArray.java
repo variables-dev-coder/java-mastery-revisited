4de529432c53b0c13457bbbc968088fdefc655cf
Munna Aziz Mondal <azizmunna661@gmail.com>
2026-09-01 18:02:37 +0530

    Revision Reverse Array example

---
+++ src/revision_Arrays/ReverseArray.java
@@ -0,0 +1,28 @@
+package revision_Arrays;
+
+import java.util.Arrays;
+
+public class ReverseArray {
+
+    public static void main(String[] args) {
+
+        int[] arr = {10, 20, 30, 40, 50};
+
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left < right) {
+
+            int temp = arr[left];
+
+            arr[left] = arr[right];
+
+            arr[right] = temp;
+
+            left++;
+            right--;
+        }
+
+        System.out.println(Arrays.toString(arr));
+    }
+}
