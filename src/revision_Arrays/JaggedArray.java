756e10779e9b8f4ec2252f2d4dc2de406782fe12
Munna Aziz Mondal <azizmunna661@gmail.com>
2026-09-02 23:30:29 +0530

    Array Revision, Jagged Array

---
+++ src/revision_Arrays/JaggedArray.java
@@ -0,0 +1,25 @@
+package revision_Arrays;
+
+public class JaggedArray {
+
+    public static void main(String[] args) {
+
+        int[][] arr = {
+                {10, 20},
+                {30, 40, 50, 60},
+                {70, 80, 90}
+        };
+
+        for (int i = 0; i < arr.length; i++) {
+
+            System.out.print("Row " + i + ": ");
+
+            for (int j = 0; j < arr[i].length; j++) {
+
+                System.out.print(arr[i][j] + " ");
+            }
+
+            System.out.println();
+        }
+    }
+}
