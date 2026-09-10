709b15311b7a844dd258c275fdd3016c5fe7c320
Munna Aziz Mondal <azizmunna661@gmail.com>
2026-09-10 13:53:07 +0530

    String, SubString example

---
+++ src/revision_String/String_LengthSubString.java
@@ -0,0 +1,13 @@
+package revision_String;
+
+public class String_LengthSubString {
+
+    public static void main(String[] args) {
+
+        String s = "Programming";
+
+        String result = s.substring(3,7);
+
+        System.out.println(result);
+    }
+}
