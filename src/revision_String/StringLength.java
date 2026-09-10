36945b97e271c2aec9fbb23fd9fff35b97ca0d72
Munna Aziz Mondal <azizmunna661@gmail.com>
2026-09-10 12:15:34 +0530

    Added, String Length basics problem

---
+++ src/revision_String/StringLength.java
@@ -0,0 +1,17 @@
+package revision_String;
+
+import java.util.Scanner;
+
+public class StringLength {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Type Any String: ");
+
+        String s = sc.nextLine();
+
+        System.out.println(s.length());
+    }
+}
